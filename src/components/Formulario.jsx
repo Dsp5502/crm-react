7d68878a8ccf618b8d0223e5778cd01845ae8497
David Puerto Guerrero <dsp5502@gmail.com>
2022-06-02 08:16:58 -0500

    Añadiendo un spinner de carga en edición

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -3,8 +3,9 @@ import { Formik, Form, Field } from 'formik';
 import { useNavigate } from 'react-router-dom';
 import * as Yup from 'yup';
 import Alerta from './Alerta';
+import Spinner from './Spinner';
 
-const Formulario = () => {
+const Formulario = ({ cargando, cliente }) => {
   const nuevaClienteSchema = Yup.object().shape({
     nombre: Yup.string()
       .min(3, 'El Nombre es muy corto')
@@ -41,19 +42,22 @@ const Formulario = () => {
       console.log(error);
     }
   };
-  return (
+  return cargando ? (
+    <Spinner />
+  ) : (
     <div className='bg-white mt-10 px-5 py-10 rounded-md shadow-md md:w-3/4 mx-auto '>
       <h1 className='text-gray-600 font-bold text-xl uppercase text-center'>
-        Agregar Cliente
+        {cliente?.nombre ? 'Editar Cliente' : 'Agregar Cliente'}
       </h1>
       <Formik
         initialValues={{
-          nombre: '',
-          empresa: '',
-          email: '',
-          telefono: '',
-          notas: '',
+          nombre: cliente?.nombre ?? '',
+          empresa: cliente?.empresa ?? '',
+          email: cliente?.email ?? '',
+          telefono: cliente?.telefono ?? '',
+          notas: cliente?.notas ?? '',
         }}
+        enableReinitialize={true}
         onSubmit={async (values, { resetForm }) => {
           await handleSubmit(values);
           resetForm();
@@ -137,7 +141,7 @@ const Formulario = () => {
             </div>
             <input
               type='submit'
-              value='Agregar Cliente'
+              value={cliente?.nombre ? 'Editar Cliente' : 'Agregar Cliente'}
               className='mt-5 w-full bg-blue-800 p-3 text-white uppercase font-bold text-lg '
             />
           </Form>
@@ -145,6 +149,10 @@ const Formulario = () => {
       </Formik>
     </div>
   );
+};
+Formulario.defaultProps = {
+  cliente: {},
+  cargando: false,
 };
 
 export default Formulario;
